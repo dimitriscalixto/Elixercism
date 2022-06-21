247a9d6ab9d21a11c4cd0bc62b8d878fd209c1ec
Dimitris Calixto <dimitrisc47@gmail.com>
2022-06-21 18:41:47 -0500

    first commit

---
+++ lasagna/lib/lasagna.ex
@@ -0,0 +1,18 @@
+defmodule Lasagna do
+    def expected_minutes_in_oven() do
+      40
+    end
+
+      def remaining_minutes_in_oven(minutes) do
+      remaining_minutes_in_oven = expected_minutes_in_oven() - minutes
+    end
+      def  preparation_time_in_minutes(layer) do
+       layer * 2
+    end
+      def total_time_in_minutes(layer, oven) do
+      total_time_in_minutes =  preparation_time_in_minutes(layer) + oven
+    end
+def alarm() do
+      "Ding!"
+    end
+end
